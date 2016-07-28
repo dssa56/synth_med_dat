9151e75b7f51aeb67d9016d5d64b71261f852873
Lawrence Phillips <lawphi56@gmail.com>
2016-07-28 14:27:08 +0100

    generating parent genotypes given child genotype. restructured code slightly

--- survive.py
+++ survive.py
@@ -5,22 +5,23 @@ sur_d = json.load(open('survival.json'))
 
 
 genes = [('BRCA1', 'BRCA2', 'N'), ('Y', 'N')]
-genotypes = [(a, b) for a in genes for b in genes]
+genotypes = [(a, b) for a in genes[0] for b in genes[1]]
 
-genotype_key = {0: 'BRCA1Y',
-                1: 'BRCA1N',
-                2: 'BRCA2Y',
-                3: 'BRCA2N',
-                4: 'NY',
-                5: 'NN'}
+
+genotype_pops = {('BRCA1', 'Y'): 0.1139*1/500,
+                 ('BRCA1', 'N'): (1 - 0.1139)*1/500,
+                 ('BRCA2', 'Y'): 0.1139*1/500,
+                 ('BRCA2', 'N'): (1 - 0.1139)*1/500,
+                 ('N', 'Y'): 0.1139*498/500,
+                 ('N', 'N'): (1 - 0.1139)*498/500}
 
 th = 13.0377
 
 event_probs = {}
 for k in sur_d.keys():
-    event_probs[k+'N'] = [1 - sur_d[k][0]] + [sur_d[k][i] - sur_d[k][i+1]
-                                              for i in range(4)]
-    event_probs[k+'Y'] = (
+    event_probs[(k, 'N')] = [1 - sur_d[k][0]] + [sur_d[k][i] - sur_d[k][i+1]
+                                                 for i in range(4)]
+    event_probs[(k, 'Y')] = (
         [1 - sur_d[k][0]**th] +
         [sur_d[k][i]**th - sur_d[k][i+1]**th for i in range(4)]
         )
@@ -31,27 +32,36 @@ def get_patient_genotype(age):
         age = 30
 
     age = age // 10 - 3
-    probs = [(0.1139)*1/500*event_probs['BRCA1Y'][age],
-             (1 - 0.1139)*1/500*event_probs['BRCA1N'][age],
-             (0.1139)*1/500*event_probs['BRCA2Y'][age],
-             (1 - 0.1139)*1/500*event_probs['BRCA2N'][age],
-             (0.1139)*498/500*event_probs['NY'][age],
-             (1 - 0.1139)*498/500*event_probs['NN'][age]]
+    probs = [genotype_pops[gn]*event_probs[gn][age]
+             for gn in genotypes]
 
     norm = sum(probs)
     probs = [p/norm for p in probs]
 
-    pop_probs = [0.1139*1/500,
-                 (1 - 0.1139)*1/500,
-                 0.1139*1/500,
-                 (1 - 0.1139)*1/500,
-                 0.1139*498/500,
-                 (1 - 0.1139)*498/500]
-
-    norm = sum(pop_probs)
-    pop_probs = [p/norm for p in pop_probs]
-
     copy_1 = np.random.choice(range(6), p=probs)
-    copy_2 = np.random.choice(range(6), p=pop_probs)
+    copy_2 = np.random.choice(range(6), p=list(genotype_pops.values()))
 
-    return (genotype_key[copy_1], genotype_key[copy_2])
+    return (genotypes[copy_1], genotypes[copy_2])
+
+
+def parent_genotype_prob(child_genotype, parent_genotypes):
+    prob = 1
+    for sex in [0, 1]:
+        for locus, cg in enumerate(child_genotype[sex]):
+            prob *= len([1 for pg in parent_genotypes[sex]
+                         if pg[locus] == cg])/2
+    return prob
+
+
+def get_parent_genotypes(child_genotype):
+    possibilities = [[(pgmm, pgmf), (pgfm, pgff)]
+                     for pgmm in genotypes for pgmf in genotypes
+                     for pgfm in genotypes for pgff in genotypes]
+    probs = [parent_genotype_prob(child_genotype, poss) *
+             genotype_pops[poss[0][0]]*genotype_pops[poss[0][1]] *
+             genotype_pops[poss[1][0]]*genotype_pops[poss[1][1]]
+             for poss in possibilities]
+    norm = sum(probs)
+    probs = [p/norm for p in probs]
+    return possibilities[np.random.choice(len(possibilities),
+                                          p=probs)]
